cf07afc443b2ede31171a2c097a5eb1ee58aadf6
freeman-lab <the.freeman.lab@gmail.com>
2022-01-04 17:28:33 -0800

    fix path

--- next.config.js
+++ next.config.js
@@ -9,23 +9,5 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['jsx', 'js', 'md', 'mdx'],
-  webpack: (config, options) => {
-    if (options.isServer) {
-      config.externals = ['react', 'theme-ui', ...config.externals]
-    }
-    config.resolve.alias['react'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'react'
-    )
-    config.resolve.alias['theme-ui'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'theme-ui'
-    )
-    return config
-  },
-  assetPrefix: isDev ? '' : 'https://compliance-users.staging.carbonplan.org',
+  assetPrefix: isDev ? '' : 'https://compliance-users.carbonplan.org',
 })
